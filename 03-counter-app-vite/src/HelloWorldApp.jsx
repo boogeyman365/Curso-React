a10ebf42ab44fdb2cf0ef57ef45497106eca4637
Boogey <oscarestablie@hotmail.com>
2022-12-20 11:23:36 +0100

    firstApp

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,12 @@
+
+
+// export function App() {
+//   // document.createElement....
+//   return (<h1>Hola Mundo!!!</h1>);
+// }
+
+export const App = () => {
+  return (
+    <h1>HelloWorld App</h1>
+  );
+}
