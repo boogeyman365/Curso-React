2e95e3867698980c430fd5c0aad9a5b56a4b281e
Oscar <oscar.establie@bosonit.com>
2023-02-28 10:53:31 +0100

    empiece useReducer

---
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,63 @@
+import { useReducer } from "react"
+import { todoReducer } from "./todoReducer"
+
+export const TodoApp = () => {
+
+  const initialState = [
+    {
+      id: new Date().getTime(),
+      description: 'Recolectar la piedra del alma',
+      done: 'false',
+    },
+    {
+      id: new Date().getTime() * 3,
+      description: 'Recolectar la piedra del poder',
+      done: 'false',
+    }
+  ]
+
+  const [state, dispatch] = useReducer( todoReducer, initialState )
+
+
+  return (
+    <>
+      <h1>Todo App (10) <small>,pendientes: 2</small> </h1>
+      <hr />
+
+      <div className="row">
+
+        <div className="col-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-danger">Borrar</button>
+            </li>
+            {/* <li className="list-group-item">Item 2</li>
+            <li className="list-group-item">Item 3</li> */}
+          </ul>
+        </div>
+
+        <div className="col-5">
+          <h4> Agregar TODO</h4>
+          <hr />
+          <form>
+            <input 
+              type="text"
+              placeholder="¿Qué hay que hacer?"
+              className="form-control"
+            />
+
+            <button type="submit" className="btn btn-outline-primary mt-2">
+              Agregar
+            </button>
+          </form>
+        </div>
+
+      </div>
+
+      
+
+      
+    </>
+  )
+}
